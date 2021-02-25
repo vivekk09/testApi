49d9d0fd0988ac9afc21b9918334e122f851f063
Sonu Kumar <sonukumar03xt@gmail.com>
2021-02-26 00:15:20 +0530

    id added

--- database.sql
+++ database.sql
@@ -1,4 +1,8 @@
-CREATE TABLE contacts(
-    name varchar(30),
-    number int
-);
+CREATE DATABASE test;
+USE test;
+CREATE TABLE `contacts` (
+  `name` varchar(45) DEFAULT NULL,
+  `id` varchar(45) NOT NULL,
+  `mobile_number` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
